26760a33023d8b27385416e3397a314daf9d0a70
Munira03 <muniraattar17@gmail.com>
2025-12-28 23:26:18 +0530

    removes rate limiter Fix CORS preflight for Render deployment

--- src/app.js
+++ src/app.js
@@ -15,7 +15,15 @@ app.use(cors({
   credentials: true
 }));
 
-
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Body parser
 app.use(express.json());
