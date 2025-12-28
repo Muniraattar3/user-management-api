ab656948ce136bfcb4033533ac116e159f968b2e
Munira03 <muniraattar17@gmail.com>
2025-12-28 17:32:58 +0530

    deploy

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected");
-    }catch (erorr){
-        console.error(erorr);
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI not defined");
+  }
+
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("MongoDB connected");
+};
 
 export default connectDB;
